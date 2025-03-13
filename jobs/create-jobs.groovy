fc7f9fb02430bd810da3b2af0dbc50fb650cffbf
Sagar Kedar <110680012+Sagar-kedar@users.noreply.github.com>
2025-03-13 15:47:54 +0530

    Update create-jobs.groovy

--- jobs/create-jobs.groovy
+++ jobs/create-jobs.groovy
@@ -1,40 +1,20 @@
-
-// Create a simple Freestyle job
-job('example-freestyle-job') {
-    description('A simple freestyle job created using Job DSL.')
-    steps {
-        shell('echo "Hello from the freestyle job!"')
-    }
-}
-
-// Create a Pipeline job
-pipelineJob('example-pipeline-job') {
-    description('A simple pipeline job created using Job DSL.')
-    definition {
-        cps {
-            script("""
-            pipeline {
-                agent any
-                stages {
-                    stage('Build') {
-                        steps {
-                            echo 'Building...'
-                        }
-                    }
-                    stage('Test') {
-                        steps {
-                            echo 'Testing...'
-                        }
-                    }
-                    stage('Deploy') {
-                        steps {
-                            echo 'Deploying...'
-                        }
-                    }
-                }
+pipeline {
+    agent any 
+    stages {
+        stage('Build') { 
+            steps {
+                echo "This is Build stage."
             }
-            """)
+        }
+        stage('Test') { 
+            steps {
+                echo "This is Test stage." 
+            }
+        }
+        stage('Deploy') { 
+            steps {
+                echo "This is Deploy stage." 
+            }
         }
     }
 }
-
