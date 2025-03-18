ad4d3c17123638b90c4b38b9514df284df5c8318
Sagar Kedar <sagarkedar@Admins-MacBook-Pro.local>
2025-03-18 13:54:43 +0530

    rm test

--- jobs/test.groovy
+++
@@ -1,9 +0,0 @@
-freestyleJob('MyJob') {
-    description('This is a freestyle job created with Job DSL')
-    
-    steps {
-        shell('echo "Stage: Build"')
-        shell('echo "Stage: Test"')
-        shell('echo "Stage: Deploy"')
-    }
-}
