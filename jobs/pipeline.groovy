20a7a57fbc18599d568a2a56e9ea1e2b1569784d
Sagar Kedar <sagarkedar@Admins-MacBook-Pro.local>
2025-03-18 12:19:03 +0530

    new pipeline job added

---
+++ jobs/pipeline.groovy
@@ -0,0 +1,37 @@
+pipelineJob('MyPipelineJob') {
+    description('This is a pipeline job created with Job DSL')
+    
+    definition {
+        cps {
+            script("""
+                pipeline {
+                    agent any
+                    
+                    stages {
+                        stage('Build') {
+                            steps {
+                                script {
+                                    sh 'echo "Stage: Build"'
+                                }
+                            }
+                        }
+                        stage('Test') {
+                            steps {
+                                script {
+                                    sh 'echo "Stage: Test"'
+                                }
+                            }
+                        }
+                        stage('Deploy') {
+                            steps {
+                                script {
+                                    sh 'echo "Stage: Deploy"'
+                                }
+                            }
+                        }
+                    }
+                }
+            """)
+        }
+    }
+}
