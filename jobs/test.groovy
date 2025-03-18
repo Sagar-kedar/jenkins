c2bae46cc14f64cd25e09f6c9d200b807509c04b
Sagar Kedar <sagarkedar@Admins-MacBook-Pro.local>
2025-03-18 12:08:46 +0530

    myvalues and jobs updated

--- jobs/test.groovy
+++ jobs/test.groovy
@@ -1,36 +1,9 @@
-job('MyPipelineJob') {
-    description('This is a pipeline job created with Job DSL')
+freestyleJob('MyJob') {
+    description('This is a freestyle job created with Job DSL')
+    
     steps {
-        // Add the pipeline script here if you want to embed it in Job DSL
-        pipelineScript("""
-        pipeline {
-            agent any
-
-            stages {
-                stage('Build') {
-                    steps {
-                        script {
-                            sh 'echo "Stage: Build"'
-                        }
-                    }
-                }
-                stage('Test') {
-                    steps {
-                        script {
-                            sh 'echo "Stage: Test"'
-                        }
-                    }
-                }
-                stage('Deploy') {
-                    steps {
-                        script {
-                            sh 'echo "Stage: Deploy"'
-                        }
-                    }
-                }
-            }
-        }
-        """)
+        shell('echo "Stage: Build"')
+        shell('echo "Stage: Test"')
+        shell('echo "Stage: Deploy"')
     }
 }
-
